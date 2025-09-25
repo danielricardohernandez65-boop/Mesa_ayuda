b9a024746879477596279d2507320c7773c46959
MontoyaN1 <juanpablomontoyajpmv@gmail.com>
2025-09-25 17:01:20 -0500

    prueba worflow

---
+++ tests/test_basic.py
@@ -0,0 +1,7 @@
+def test_example():
+    """Test básico que siempre pasa - para probar el workflow"""
+    assert 1 + 1 == 2
+
+def test_another_example():
+    """Otro test simple"""
+    assert "hello".upper() == "HELLO"
